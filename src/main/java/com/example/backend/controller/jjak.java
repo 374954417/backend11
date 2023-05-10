9a7d49c577ab848010f5ed03e1dcfd0b63a428d8
374954417 <374954417@qq.com>
2023-05-10 13:25:19 +0800

    wjj

---
+++ src/main/java/com/example/backend/controller/jjak.java
@@ -0,0 +1,9 @@
+package com.example.backend.controller;
+
+public class jjak {
+    public int c=6;
+}
+
+
+
+
--- src/main/java/com/example/backend/controlleraa.java
+++ src/main/java/com/example/backend/controlleraa.java
@@ -1,5 +1,12 @@
 package com.example.backend;
 
+import com.example.backend.controller.jjak;
+
 public class controlleraa {
     int a;
+    void us()
+    {
+        jjak jj=new jjak();
+        jj.c=9;
+    }
 }
